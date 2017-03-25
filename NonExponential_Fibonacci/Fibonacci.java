388e7fdc3aeb4e55663bbccf48d476bdca7a2e19
Aman Gudluri <amangudluri@yahoo.com>
2017-03-25 16:09:47 -0500

    Update Fibonacci.java

--- NonExponential_Fibonacci/Fibonacci.java
+++ NonExponential_Fibonacci/Fibonacci.java
@@ -15,9 +15,9 @@ public class Fibonacci {
 		
 		final int MAX_N = 92; // because f(92) exceeds the maximum length of long
 		final String ERROR_MESSAGE = "\nThe number entered must be " + "greater than 0 and at most " + MAX_N + "."; //the error message in case 
-																													//the input is entered
+															    //the input is entered
 		if (n <= 0 || n > MAX_N)
-			throw new IllegalArgumentException(ERROR_MESSAGE); //The illegal argument exception that shows you the erro message
+			throw new IllegalArgumentException(ERROR_MESSAGE); //The illegal argument exception that shows you the error message
 		if (n == 1) return previous; //Conditions to proceed with the function
 		if (n == 2) return current;
 		return f(n-1,current,current+previous); //the recursive call with the reduced n and a single-window shifted 'previous' and 'current'
@@ -25,11 +25,11 @@ public class Fibonacci {
 
 	public static void main(String[] args) {
 		System.out.println("Enter 'n' and the first and second numbers in the sequence seperated by commas"); //Displaying message 
-																						//for taking in the input from the user
+														      //for taking in the input from the user
 		Scanner sc = new Scanner(System.in);
 		String[] line = sc.nextLine().split(","); //Reading the input and splitting it at the commas
 		System.out.println(f(Integer.parseInt(line[0]),Long.parseLong(line[1]),Long.parseLong(line[2]))); //Passing the split inputs to our	
-																											//fibonacci function
+														  //fibonacci function
 		sc.close();
 	}
 }
